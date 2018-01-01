da056c6cc1181919838b765cdccdef970407b9b3
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-01-01 16:07:50 -0200

    Create and migrate db

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
 
 #Create to interpreter of python, control the all aplication
 app = Flask(__name__)
@@ -8,5 +12,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
 db = SQLAlchemy(app)
 
+#Create instance migrate - Aplication and my DataBase
+migrate = Migrate(app, db)
+
+#Create instance manager - Commands for initializing
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
--- Flask/run.py
+++ Flask/run.py
@@ -1,7 +1,10 @@
-from app import app
+#from app import app
+from app import manager
 
 if __name__== "__main__":
-    app.run()
+    manager.run()
+    #if not Manager
+    #app.run()
 
 # Após executado aqui, ele cria uma "váriavel global" app,
 # depois procura o __init__ dentro a pasta app
