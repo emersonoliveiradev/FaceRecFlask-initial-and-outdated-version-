14b434c64bf4c7cbcf9f50a0e5c16d484af73196
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-01-01 15:32:42 -0200

    Install 'migrate' and 'script'

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 #Create to interpreter of python, control the all aplication
 app = Flask(__name__)
+
+#Create config of db and db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+
 
 from app.controllers import default
