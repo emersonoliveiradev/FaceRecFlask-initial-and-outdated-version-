96bf5d8a4c7090388dcc352a90f3f9d8e2d3b1cd
EmersonPereiraOliveira <emersonhaw@gmail.com>
2018-05-30 18:33:02 -0300

    Reformulação dotemplate principal

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 #Create to interpreter of python, control the all aplication
@@ -16,5 +16,7 @@ migrate = Migrate(app, db)
 #Create instance manager - Commands for initializing
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+manager.add_command('runserver', Server(host="0.0.0.0", port=8000))
 
 from app.controllers import default
+from app.models import tables
