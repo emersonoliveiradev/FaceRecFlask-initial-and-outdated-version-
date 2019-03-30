e17f141e87413a04ce00c18b47023baffcfe486e
EmersonPereiraOliveira <emersonhaw@gmail.com>
2019-03-30 20:05:46 -0300

    Último atualização neste repositótrio

--- Flask/pasta_dos_usuarios/__init__.py
+++ Flask/pasta_dos_usuarios/__init__.py
@@ -12,3 +12,5 @@ from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
 from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
 from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
 from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
+from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
+from .u_1_Emerson.algoritmos.auxiliar import ReconhecimentoFacial
